b39641c2fa8b10fe67038792ea82c765112487db
corbin <me@corbin.ch>
2023-08-19 10:44:59 -0400

    switch from dmenuumount to new unmounter script

--- config.h
+++ config.h
@@ -262,7 +262,7 @@ static Key keys[] = {
     {MODKEY,             XK_F7, spawn, {.v = (const char *[]){"td-toggle", NULL}}},
     {MODKEY,             XK_F8, spawn, {.v = (const char *[]){"mw", "-Y", NULL}}},
     {MODKEY,             XK_F9, spawn, {.v = (const char *[]){"dmenumount", NULL}}},
-    {MODKEY,             XK_F10, spawn, {.v = (const char *[]){"dmenuumount", NULL}}},
+    {MODKEY,             XK_F10, spawn, {.v = (const char *[]){"unmounter", NULL}}},
     {MODKEY,             XK_F11, spawn,
      SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings "
            "--profile=low-latency --input-conf=/dev/null --title=webcam $(ls "
