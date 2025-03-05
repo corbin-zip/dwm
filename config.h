c1bef5aae324ad24dafd83c271f710c2f6b305e3
corbin <me@corbin.ch>
2025-03-05 15:45:02 -0500

    swap to ioveska font
    thinner but still easy to read. allows for more text along the status
    bar

--- config.h
+++ config.h
@@ -20,8 +20,9 @@ static int smartgaps = 1; /* 1 means no outer gap when there is only one window 
 static int showbar = 1; /* 0 means no bar */
 static int topbar = 1;  /* 0 means bottom bar */
 static char *fonts[] = {
-    "monospace:size=10",
-    "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"
+    /* "monospace:size=12", */
+    "Iosevka:style:medium:size=12",
+    "NotoColorEmoji:pixelsize=14:antialias=true:autohint=true"
     };
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#444444";
